15f89c71dbfcd77ab527f6b3bf58a474d569816b
Roman Pulyakho <pulromans@gmail.com>
2023-10-10 01:15:43 +0300

    fix email_sender.py

--- app/users/email_sender.py
+++ app/users/email_sender.py
@@ -6,8 +6,7 @@ def send_confirmation_email(*emails, subject: str, message: str):
     email = EmailMessage(subject=subject,
                          body=message,
                          from_email=EMAIL_HOST_USER,
-                         to=[emails],
-                         headers={'From': f'{EMAIL_HOST_USER}'})
+                         to=emails)
 
     email.send()
 
